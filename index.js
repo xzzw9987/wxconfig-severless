984adefd23616ef602efd027c2f487ee7f13c4a8
辛忠柱 <xzzw9987@gmail.com>
2023-03-09 14:43:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,95 @@ router.get("/api/wx_openid", async (ctx) => {
   }
 });
 
+var appid = 'wx875779ce30627de4'
+var secret = processArgs['secret']
+
+// wx875779ce30627de4
+// 23d2c4a39ae964cccf54979a96c6de24
+var accessToken
+var jsApiTicket
+var timestamp
+
+console.log(appid, secret)
+
+function getAccessToken () {
+  https.get({
+    hostname: 'api.weixin.qq.com',
+    path: '/cgi-bin/token?grant_type=client_credential&appid=' + appid + '&secret=' + secret
+  }, function (res) {
+    var d = ''
+    res.setEncoding('utf8')
+    res.on('data', function (data) {
+      d += data
+    })
+    res.on('end', function () {
+      d = JSON.parse(d)
+      accessToken = d['access_token']
+      if (accessToken)
+        getJSAPITicket()
+    })
+  })
+  setTimeout(getAccessToken, 7200 * 1000)
+}
+
+function getJSAPITicket () {
+  // https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=ACCESS_TOKEN&type=jsapi
+  https.get({
+      hostname: 'api.weixin.qq.com',
+      path: '/cgi-bin/ticket/getticket?type=jsapi'
+    },
+    function (res) {
+      var d = ''
+      res.setEncoding('utf8')
+      res.on('data', function (data) {
+        d += data
+      })
+      res.on('end', function () {
+        d = JSON.parse(d)
+        jsApiTicket = d['ticket']
+      })
+    }
+  )
+
+}
+
+function getSignature (url) {
+  if (!jsApiTicket)
+    return
+  timestamp = parseInt(('' + (new Date).getTime()).substring(0, 10), 10)
+  console.log(
+    'jsapi_ticket=' + jsApiTicket +
+    '&noncestr=' + nonceStr +
+    '&timestamp=' + timestamp +
+    '&url=' + url
+  )
+  return crypto.createHash('sha1')
+    .update(
+      'jsapi_ticket=' + jsApiTicket +
+      '&noncestr=' + nonceStr +
+      '&timestamp=' + timestamp +
+      '&url=' + url
+    )
+    .digest('hex')
+}
+
+router.get('/wx_config', async (ctx, next) => {
+  const {url} = ctx.query
+  var sig = getSignature(url)
+  const content =
+    'wx.config({' +
+    'debug: false, ' +
+    'appId: \'' + appid + '\',' +
+    'timestamp: \'' + timestamp + '\',' +
+    'nonceStr: \'' + nonceStr + '\',' +
+    'signature: \'' + sig + '\',' +
+    'jsApiList: [\'onMenuShareTimeline\',\'onMenuShareAppMessage\']' +
+    '})'
+  ctx.response.headers['content-type'] = 'application/javascript; charset=utf-8'
+  ctx.body = content;
+})
+
+
 const app = new Koa();
 app
   .use(logger())
@@ -59,6 +148,7 @@ app
 
 const port = process.env.PORT || 80;
 async function bootstrap() {
+  getJSAPITicket()
   await initDB();
   app.listen(port, () => {
     console.log("启动成功", port);
